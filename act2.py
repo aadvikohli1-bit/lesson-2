744040970513e2b5ea9791249c501f46eb8f895a
aadvikohli1-bit <aadvikohli1@gmail.com>
2025-11-08 10:42:33 -0800

    commit1

---
+++ act2.py
@@ -0,0 +1,6 @@
+x=2
+inn=float(x)
+print(inn)
+y=2.09
+inn1=int(y)
+print(inn)
---
+++ main.py
@@ -0,0 +1,8 @@
+x = 2.8
+y = True
+T = "Welcome"
+u = -2
+print(type (x))
+print(type (y))
+print(type (T))
+print(type (u))
